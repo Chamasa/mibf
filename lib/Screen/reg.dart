f50eb4bef910c62164461091683dfae9880ead6c
Khachachart Khongkhot <88022973+Chamasa@users.noreply.github.com>
2021-09-14 15:42:16 +0700

    Add files via upload

--- lib/Screen/reg.dart
+++ lib/Screen/reg.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fmc/contant.dart';
+
+var name, age, surname, email, password;
+final formkey = GlobalKey<FormState>();
 
 class Register extends StatefulWidget {
-  const Register({ Key? key }) : super(key: key);
+  const Register({Key? key}) : super(key: key);
 
   @override
   _RegisterState createState() => _RegisterState();
@@ -10,8 +14,140 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Register'),),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Register'),
+        ),
+        body: Form(
+          key: formkey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                txtName(),
+                txtSurname(),
+                txtEmail(),
+                txtPassword(),
+                btnS(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+  Widget txtName() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+        style: TextStyle(
+          color: sColor,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          labelText: "ชื่อ",
+          icon: Icon(Icons.person),
+          hintText: "ใส่ชื่อ",
+        ),
+        keyboardType: TextInputType.name,
+        validator: (val) {
+          if (val!.length < 3) {
+            return 'กรุญากรอกชื่อให้มากกว่า 3 ตัวอักษร';
+          }
+        },
+        onSaved: (val) {
+          name = (val);
+        },
+      ),
+    );
+  }
+
+  Widget txtSurname() {
+    return Container(
+        margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+        child: TextFormField(
+          style: TextStyle(
+            color: sColor,
+            fontSize: 24,
+          ),
+          decoration: InputDecoration(
+            labelText: "นามสกุล",
+            icon: Icon(Icons.person),
+            hintText: "ใส่นามสกุล",
+          ),
+          keyboardType: TextInputType.name,
+          validator: (val2) {
+            if (val2!.isEmpty) {
+              return 'กรุญากรอกนามสกุล';
+            } else if (val2.length < 3) {
+              return 'กรุญากรอกชื่อให้มากกว่า 3 ตัวอักษร';
+            }
+          },
+          onSaved: (val2) {
+            surname = (val2);
+          },
+        ));
+  }
+
+  Widget txtEmail() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+          style: TextStyle(
+            color: sColor,
+            fontSize: 24,
+          ),
+          decoration: InputDecoration(
+            labelText: "Email",
+            icon: Icon(Icons.email),
+          ),
+          keyboardType: TextInputType.emailAddress,
+          validator: (val3) {
+            if (!(val3!.contains('@'))) {
+              return 'กรุญากรอก Email';
+            }else if(!(val3.contains('.'))){
+              return 'กรุญากรอก Email';
+            }
+          }, 
+          onSaved: (val3){
+          email = (val3);}
+          ),
+    );
+  }
+
+  Widget txtPassword() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+        style: TextStyle(
+          color: sColor,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          labelText: "Password",
+          icon: Icon(Icons.password),
+          hintText: "ใส่หรัสผ่าน",
+        ),
+        obscureText: true,
+        onSaved: (val4) {
+        password = (val4);}
+      ),
+    );
+  }
+}
+
+Widget btnS() {
+  return ElevatedButton(
+      child: Text('Submit'),
+      onPressed: () {
+        print("object");
+        if (formkey.currentState!.validate()) {
+          formkey.currentState!.save();
+          print('ชื่อ : $name');
+          print('นามสกุล : $surname');
+          print('Email : $email');
+          print('รหัสผ่าน : $password');
+        }
+      });
 }
--- lib/contant.dart
+++ lib/contant.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const pColor = Color (0xFFD3171E);
-const sColor = Color (0xffe194);
+const sColor = Color.fromARGB(100, 255, 0, 0);
